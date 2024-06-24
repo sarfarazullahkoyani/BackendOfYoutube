3d77f0eb5f7b68c620a37f6e87ce60130c20f7d6
sarfarazullahkoyani <sarfarazullahkoyani@gmail.com>
2024-06-24 17:50:13 +0500

    upload middleware, router and controllers

--- src/app.js
+++ src/app.js
@@ -27,4 +27,10 @@ app.use(
 // this skims out the data in the url sent by users
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+// routes declaration
+app.use("/api/v1/users", userRouter);
+// http://localhost:8000/api/v1/users/register
 export { app };
